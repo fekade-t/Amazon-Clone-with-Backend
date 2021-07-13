c3c9adc5af6e9e9b639c964e0b27afebfb7fe280
Fekade Tamiru <80615505+fekade-t@users.noreply.github.com>
2021-07-13 00:02:17 -0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://us-central1-clone-180c1.cloudfunctions.net/api',
+  baseURL: 'baseurl',
 });
 
 export default instance;
-// http://localhost:5001/clone-180c1/us-central1/api localhost firebase
-// https://us-central1-clone-180c1.cloudfunctions.net/api
